d5b7b6f5dd4c746f4797e6aff13b9ee5768b9e6b
Pep Guiem <pepguiemnolla@gmail.com>
2023-11-11 17:53:13 +0100

    finish

--- src/js/script.js
+++ src/js/script.js
@@ -1,107 +1,25 @@
+const caballero = new Image();
+const caballeroRight = new Image();
+const caballeroLeft = new Image();
+const cofre = new Image();
+const paredes = new Image();
+const suelo = new Image();
+let controladorToggle = 0;
+let pageCurriculum = 0;
 var principal = document.querySelector(".lamp-content1");
 var principal2 = document.querySelector(".game")
 var onlyCurriculun = document.querySelector(".lamp-onlyCurriculum");
-let controladorToggle = 0;
-let pageCurriculum = 0;
 var curriculum1 = document.querySelector(".div-content-onlyCurriculum.content1");
 var curriculum2 = document.querySelector(".div-content-onlyCurriculum.content2");
 var curriculum3 = document.querySelector(".div-content-onlyCurriculum.content3");
 var next = document.getElementById("next");
 var play = document.querySelector(".button-play");
 var tv = document.querySelector(".tv");
-var divTv = document.querySelector(".div-tv")
-
-document.getElementById("menu").addEventListener("click", function() {
-    var menuContent = this.querySelector(".dropdown-content");
-    menuContent.classList.toggle("active");
-});
-
-document.getElementById("only-curriculum").addEventListener("click", function(){
-    displayOnlyCurriculum();
-});
-
-function displayOnlyCurriculum(){
-    principal.classList.toggle("desactive");
-    principal2.classList.toggle("desactive");
-    onlyCurriculun.classList.toggle("active");
-    if(controladorToggle == 1){
-        principal.classList.toggle("active");
-        principal2.classList.toggle("active");
-        onlyCurriculun.classList.toggle("desactive");
-        controladorToggle--;
-    }
-}
-
-document.getElementById("back").addEventListener("click", function(){
-    pageCurriculum--;
-    if(pageCurriculum < 0){
-        principal.classList.toggle("desactive");
-        principal2.classList.toggle("desactive");
-        onlyCurriculun.classList.toggle("active");
-        principal.classList.toggle("active");
-        principal2.classList.toggle("active");
-        onlyCurriculun.classList.toggle("desactive");
-        pageCurriculum = 0;
-        controladorToggle++;
-    }else if(pageCurriculum == 0){
-        curriculum1.style.display = "block";
-        curriculum2.style.display = "none";
-    } else if(pageCurriculum == 1){
-        curriculum2.style.display = "block";
-        curriculum3.style.display = "none";
-        next.style.display = "block";
-    }
-});
-
-next.addEventListener("click", function(){
-    pageCurriculum++;
-    if(pageCurriculum == 1){
-        curriculum1.style.display = "none";
-        curriculum2.style.display = "block";
-    }else if(pageCurriculum == 2){
-        curriculum2.style.display = "none";
-        curriculum3.style.display = "block";
-        next.style.display = "none";
-    }
-});
-
-var playing = false;
-play.addEventListener("click", function(){
-    tv.classList.toggle("game-active");
-    divTv.classList.toggle("game-active");
-    play.style.display = "none";
-    document.querySelector("#tablero").style.display = "block";
-    document.querySelector(".home").style.display = "block";
-    document.querySelector(".back-game").style.display = "block";
-    document.querySelector(".next-level").style.display = "none";
-    document.querySelector(".light-content1").style.display = "none";
-    document.querySelector(".light-content2").style.display = "block";
-    game();
-    whoLevelIts();
-    playing = true;
-    if(level != 1){
-        document.querySelector(".light-content2").style.display = "none";
-    }
-});
-
-
+var divTv = document.querySelector(".div-tv");
 var tablero = document.querySelector("#tablero");
 var contexto = tablero.getContext("2d");
-contexto.fillStyle = "blue";
 var horizontal = 10;
 var vertical = 5;
-const caballero = new Image();
-caballero.src = document.querySelector(".caballero").src;
-const caballeroRight = new Image();
-caballeroRight.src = document.querySelector(".caballero-right").src;
-const caballeroLeft = new Image();
-caballeroLeft.src = document.querySelector(".caballero-left").src;
-const cofre = new Image();
-cofre.src = document.querySelector(".chest").src;
-const paredes = new Image();
-paredes.src = document.querySelector(".wall").src;
-const suelo = new Image();
-suelo.src = document.querySelector(".street").src;
 var level = 1;
 var level1 = [
     ["X","X","X","X","X","X","X","X","X","X"],
@@ -161,6 +79,80 @@ var level7 = [
 var posPersonLevel7 = [4,8];
 var posPerson = posPersonLevel1;
 var levelMap = level1;
+var playing = false;
+var gw = 0;
+var gh = 0;
+caballero.src = document.querySelector(".caballero").src;
+caballeroRight.src = document.querySelector(".caballero-right").src;
+caballeroLeft.src = document.querySelector(".caballero-left").src;
+cofre.src = document.querySelector(".chest").src;
+paredes.src = document.querySelector(".wall").src;
+suelo.src = document.querySelector(".street").src;
+
+document.getElementById("menu").addEventListener("click", function() {
+    var menuContent = this.querySelector(".dropdown-content");
+    menuContent.classList.toggle("active");
+});
+
+document.getElementById("only-curriculum").addEventListener("click", function(){
+    displayOnlyCurriculum();
+});
+
+
+document.querySelector(".home").addEventListener("click", function(){
+    undisplayGame();
+});
+
+document.getElementById("back").addEventListener("click", function(){
+    pageCurriculum--;
+    if(pageCurriculum < 0){
+        principal.classList.toggle("desactive");
+        principal2.classList.toggle("desactive");
+        onlyCurriculun.classList.toggle("active");
+        principal.classList.toggle("active");
+        principal2.classList.toggle("active");
+        onlyCurriculun.classList.toggle("desactive");
+        pageCurriculum = 0;
+        controladorToggle++;
+    }else if(pageCurriculum == 0){
+        curriculum1.style.display = "block";
+        curriculum2.style.display = "none";
+    } else if(pageCurriculum == 1){
+        curriculum2.style.display = "block";
+        curriculum3.style.display = "none";
+        next.style.display = "block";
+    }
+});
+
+next.addEventListener("click", function(){
+    pageCurriculum++;
+    if(pageCurriculum == 1){
+        curriculum1.style.display = "none";
+        curriculum2.style.display = "block";
+    }else if(pageCurriculum == 2){
+        curriculum2.style.display = "none";
+        curriculum3.style.display = "block";
+        next.style.display = "none";
+    }
+});
+
+play.addEventListener("click", function(){
+    tv.classList.toggle("game-active");
+    divTv.classList.toggle("game-active");
+    play.style.display = "none";
+    document.querySelector("#tablero").style.display = "block";
+    document.querySelector(".home").style.display = "block";
+    document.querySelector(".back-game").style.display = "block";
+    document.querySelector(".next-level").style.display = "none";
+    document.querySelector(".light-content1").style.display = "none";
+    document.querySelector(".light-content2").style.display = "block";
+    game();
+    whoLevelIts();
+    playing = true;
+    if(level != 1){
+        document.querySelector(".light-content2").style.display = "none";
+    }
+});
 
 document.addEventListener('keydown', function(event) {
     let tecla = event.key.toLowerCase();
@@ -211,6 +203,93 @@ document.querySelector(".projects-buttton").addEventListener("click", function()
     next.style.display = "none";
     document.querySelector(".projects-buttton").style.display = "none";
 });
+
+document.querySelector(".next-level").addEventListener("click", function(){
+    contexto.clearRect(0, 0, tablero.width, tablero.height);
+    document.querySelector(".next-level").style.display = "none";
+    level = level + 1;
+    whoLevelIts();
+    pintarTablero();
+});
+
+document.querySelector(".back-game").addEventListener("click", function(){
+    contexto.clearRect(0, 0, tablero.width, tablero.height);
+    level = level - 1;
+    if(level == 0){
+        undisplayGame();
+        level = 1;      
+    }else{
+        whoLevelIts();
+        pintarTablero();
+    }
+});
+
+function undisplayGame(){
+    tv.classList.toggle("game-active");
+    divTv.classList.toggle("game-active");
+    play.style.display = "block";
+    document.querySelector("#tablero").style.display = "none";
+    document.querySelector(".home").style.display = "none";
+    document.querySelector(".back-game").style.display = "none";
+    document.querySelector(".next-level").style.display = "none";
+    document.querySelector(".projects-buttton").style.display = "none";
+    contexto.clearRect(0, 0, tablero.width, tablero.height);
+    document.querySelector(".light-content1").style.display = "block";
+    document.querySelector(".light-content2").style.display = "none";
+    document.querySelector(".light-content3").style.display = "none";
+    document.querySelector(".light-content4").style.display = "none";
+    document.querySelector(".light-content5").style.display = "none";
+    document.querySelector(".light-content6").style.display = "none";
+    document.querySelector(".light-content7").style.display = "none";
+    document.querySelector(".light-content8").style.display = "none";
+    playing = false;
+}
+
+function game(){
+    gw = (tablero.getBoundingClientRect().width / horizontal);
+    gh = (tablero.getBoundingClientRect().height / vertical);
+    tablero.width = gw * horizontal;
+    tablero.height = gh * vertical;
+    pintarTablero();
+}
+
+function pintarTablero(){
+    let y = 0;
+    for(let i = 0; i < vertical; i++){
+        pintarFila(i,y);
+        y = y + gh;
+    }
+}
+
+function pintarFila(i, y) {
+    let x = 0;
+    for (let j = 0; j < horizontal; j++) {
+        if (levelMap[i][j] == "X") {
+            contexto.drawImage(paredes, x, y, gw, gh); 
+        }else {
+            contexto.drawImage(suelo, x, y, gw, gh); 
+            if (levelMap[i][j] == "!") {
+                contexto.drawImage(caballero, x, y, gw + 10, gh + 15.6); 
+            } else if (levelMap[i][j] == "#") {
+                contexto.drawImage(cofre, x, y, gw + 10, gh); 
+            } 
+        }
+        contexto.rect(x, y, gw, gh);
+        x = x + gw;
+    }
+}
+
+function displayOnlyCurriculum(){
+    principal.classList.toggle("desactive");
+    principal2.classList.toggle("desactive");
+    onlyCurriculun.classList.toggle("active");
+    if(controladorToggle == 1){
+        principal.classList.toggle("active");
+        principal2.classList.toggle("active");
+        onlyCurriculun.classList.toggle("desactive");
+        controladorToggle--;
+    }
+}
 
 function whoLevelIts(){
     if(level == 1){
@@ -274,87 +353,3 @@ function movimentJugadorAD(posPerson,levelMap,moviment,caballeroImg, gwPlus){
     levelMap[posPerson[0]][posPerson[1]] = "!";
     contexto.drawImage(caballeroImg,(posPerson[1] * gw) - gwPlus, posPerson[0] * gh, gw + 10, gh + 15.6);
 }
-
-
-document.querySelector(".next-level").addEventListener("click", function(){
-    contexto.clearRect(0, 0, tablero.width, tablero.height);
-    document.querySelector(".next-level").style.display = "none";
-    level = level + 1;
-    whoLevelIts();
-    pintarTablero();
-});
-
-document.querySelector(".back-game").addEventListener("click", function(){
-    contexto.clearRect(0, 0, tablero.width, tablero.height);
-    level = level - 1;
-    if(level == 0){
-        undisplayGame();
-        level = 1;      
-    }else{
-        whoLevelIts();
-        pintarTablero();
-    }
-});
-
-function undisplayGame(){
-    tv.classList.toggle("game-active");
-    divTv.classList.toggle("game-active");
-    play.style.display = "block";
-    document.querySelector("#tablero").style.display = "none";
-    document.querySelector(".home").style.display = "none";
-    document.querySelector(".back-game").style.display = "none";
-    document.querySelector(".next-level").style.display = "none";
-    document.querySelector(".projects-buttton").style.display = "none";
-    contexto.clearRect(0, 0, tablero.width, tablero.height);
-    document.querySelector(".light-content1").style.display = "block";
-    document.querySelector(".light-content2").style.display = "none";
-    document.querySelector(".light-content3").style.display = "none";
-    document.querySelector(".light-content4").style.display = "none";
-    document.querySelector(".light-content5").style.display = "none";
-    document.querySelector(".light-content6").style.display = "none";
-    document.querySelector(".light-content7").style.display = "none";
-    document.querySelector(".light-content8").style.display = "none";
-    playing = false;
-}
-
-var gw = 0;
-var gh = 0;
-function game(){
-    gw = (tablero.getBoundingClientRect().width / horizontal);
-    gh = (tablero.getBoundingClientRect().height / vertical);
-    tablero.width = gw * horizontal;
-    tablero.height = gh * vertical;
-    pintarTablero();
-}
-
-function pintarTablero(){
-    let y = 0;
-    for(let i = 0; i < vertical; i++){
-        pintarFila(i,y);
-        y = y + gh;
-    }
-}
-
-function pintarFila(i, y) {
-    let x = 0;
-    for (let j = 0; j < horizontal; j++) {
-        if (levelMap[i][j] == "X") {
-            contexto.drawImage(paredes, x, y, gw, gh); 
-        }else {
-            contexto.drawImage(suelo, x, y, gw, gh); 
-            if (levelMap[i][j] == "!") {
-                contexto.drawImage(caballero, x, y, gw + 10, gh + 15.6); 
-            } else if (levelMap[i][j] == "#") {
-                contexto.drawImage(cofre, x, y, gw + 10, gh); 
-            } 
-        }
-        contexto.rect(x, y, gw, gh);
-        x = x + gw;
-    }
-}
-
-
-
-document.querySelector(".home").addEventListener("click", function(){
-    undisplayGame();
-});
